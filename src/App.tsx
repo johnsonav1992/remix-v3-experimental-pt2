a67e095c7a1c907010c9b9f7876dcb6833c8ec8d
Alex Johnson <johnsonav1992@gmail.com>
2026-01-11 06:21:59 -0600

    this is freaking cool

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
-export function App() {
+import type { Handle } from "@remix-run/component";
+import TodoList from "./components/TodoList/TodoList";
+import { AppContext } from "./context/context";
+
+export function App(this: Handle<AppContext>) {
+	const ctx = new AppContext();
+
+	this.context.set(ctx);
+
 	return (
-		<div>
-			<h1>Hello World</h1>
-		</div>
+		<>
+			<h1>Todo List</h1>
+			<input
+				type="text"
+				placeholder="New todo"
+				on={{
+					keydown: (event) => {
+						if (event.key === "Enter") {
+							const input = event.target as HTMLInputElement;
+							if (input.value.trim() !== "") {
+								ctx.addTodo(input.value.trim());
+								input.value = "";
+							}
+						}
+					},
+				}}
+			/>
+			<TodoList />
+		</>
 	);
 }
---
+++ src/components/TodoList/TodoList.tsx
@@ -0,0 +1,35 @@
+/** biome-ignore-all lint/a11y/useButtonType: <explanation> */
+import type { Handle } from "@remix-run/component";
+import { App } from "../../App";
+
+export default function TodoList(this: Handle) {
+	const ctx = this.context.get(App);
+
+	this.on(ctx, {
+		change: (e) => {
+			this.update();
+			console.log(e);
+		},
+	});
+
+	return () => {
+		console.log("List render");
+		return (
+			<ul>
+				{ctx.todos.map((todo) => (
+					<li key={todo.id}>
+						<input
+							type="checkbox"
+							checked={todo.completed}
+							on={{ change: () => ctx.toggleTodo(todo.id) }}
+						/>
+						{todo.title}
+						<button on={{ click: () => ctx.deleteTodo(todo.id) }}>
+							Delete
+						</button>
+					</li>
+				))}
+			</ul>
+		);
+	};
+}
